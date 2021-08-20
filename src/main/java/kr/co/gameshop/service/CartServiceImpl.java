3e24d687a728504fe321608b13e6724d0703d6be
jihyeon <wawa3464@gmail.com>
2021-08-20 23:38:50 +0900

    장바구니

---
+++ src/main/java/kr/co/gameshop/service/CartServiceImpl.java
@@ -0,0 +1,36 @@
+package kr.co.gameshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.gameshop.dao.CartDAO;
+import kr.co.gameshop.vo.Cart;
+
+@Service
+public class CartServiceImpl implements CartService{
+	@Autowired
+	CartDAO cartDAO;
+	
+	@Override
+	public List selectAll(int mem_id) {
+		return cartDAO.selectAll(mem_id);
+	}
+
+	@Override
+	public void insert(Cart cart) {
+		cartDAO.insert(cart);
+	}
+
+	@Override
+	public void delete(Cart cart) {
+		cartDAO.delete(cart);
+	}
+
+	@Override
+	public void deleteAll(int mem_id) {
+		cartDAO.deleteAll(mem_id);
+	}
+
+}
